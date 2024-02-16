c5d6ae70b8e5f3a289baf6ce7a8cdc7d60ca75a5
lysbrodya <lysbrodya@gmail.com>
2024-02-16 17:26:34 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Children, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// const handleClick = (e) => {
+//   setCount(count + 1);
+// };
+const Options = ({ children, onHandleClick }) => {
+  return (
+    <div>
+      <button onClick={onHandleClick}> {children}</button>
+    </div>
+  );
+};
 
+const Feedback = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p></p>
+      <p></p>
+    </div>
+  );
+};
+
+export default function App() {
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+
+  const [states, setStates] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (key) => {
+    setStates({ ...states, [key]: states[key] + 1 });
+  };
   return (
     <>
+      {" "}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Sip Happens Café</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Please leave your feedback about our service by selecting one of the
+          options below.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Options good={states.good} onHandleClick={() => updateFeedback("good")}>
+        Good
+      </Options>
+      <Options
+        neutral={states.neutral}
+        onHandleClick={() => updateFeedback("neutral")}
+      >
+        Neutral
+      </Options>
+      <Options bad={states.bad} onHandleClick={() => updateFeedback("bad")}>
+        Bad
+      </Options>
+      <Feedback
+        good={states.good}
+        neutral={states.neutral}
+        bad={states.bad}
+      ></Feedback>
     </>
-  )
+  );
 }
-
-export default App
